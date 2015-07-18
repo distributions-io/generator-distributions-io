a3108001a4f34d2b287f613065b27a7a49e59752
Planeshifter <pburckhardt@outlook.com>
2015-07-18 15:59:11 +0200

    [UPDATE] R test files

---
+++ pdf/templates/test/_test.partial.R
@@ -0,0 +1,17 @@
+options( digits = 16 );
+library( jsonlite );
+
+
+<%= parameters.map( function( p ) { return p.name + ' = ' + p.default } ).join( '\n' ) %>
+x = c( -5, -2.5, 0, 2.5, 5 )
+y = <%= rName %>( x, <%= parameters.map( function( p ) { return p.name } ).join( ',' ) %> )
+
+cat( y, sep = ",\n" )
+
+data = list(
+	<%= parameters.map( function( p ) { return p.name + ' = ' + p.name } ).join( ',\n' ) %>,
+	data = x,
+	expected = y
+)
+
+write( toJSON( data, digits = 16, auto_unbox = TRUE ), "./test/json/partial.json" )
